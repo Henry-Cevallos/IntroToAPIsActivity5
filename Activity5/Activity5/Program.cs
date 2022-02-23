600bf2a634c404d21e7de57f26c2b2a771fa8443
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2022-02-22 20:30:57 -0500

    wrote base pokemon implementation

---
+++ Activity5/Activity5/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Activity5
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a pokemon Name. Enter nothing to stop");
+
+                    var name = Console.ReadLine();
+
+                    if(string.IsNullOrEmpty(name))
+                    {
+                        break;
+                    }
+
+                    var result = await client.GetAsync("https://pokeapi.co/api/v2/pokemon/" + name.ToLower());
+                    var resultRead = await result.Content.ReadAsStringAsync();
+
+                    var pokemon = JsonConvert.DeserializeObject<Pokemon>(resultRead);
+
+                    Console.WriteLine("Pokemon Name: " + pokemon.Name);
+                    Console.WriteLine("Pokemon Id: " + pokemon.Id);
+                }
+                catch(Exception)
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+        }
+    }
+
+    class Pokemon
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        
+
+    }
+}
